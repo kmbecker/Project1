4193d8369d048085f87bd75a6b6dad21b0b3f621
Seth Walton <sdwalton83@gmail.com>
2017-11-10 00:14:02 -0600

    cleaned up comments in java script and fixed a bug with the suggestions

--- javascript.js
+++ javascript.js
@@ -1,24 +1,17 @@
+//  ---------------- Initialize Firebase ---------------!!!!!
 // // Initialize Firebase
 var url ="https://movieproject-fc07e.firebaseio.com";
 var dataRef = new Firebase(url);
 var getKey;
 
-// ------------ Firebase 2nd Suggestions _--------!
+//  ---------------- Firebase 2nd Suggestions ---------------!!!!!
+
 var url2 ="https://test-f0675.firebaseio.com";
 var dataRef2 = new Firebase(url2);
 var getKey2;
-  // Initialize Firebase
-  // var config = {
-  //   apiKey: "AIzaSyDHolWI_ZAvFdCLyYUFDiSNnsWLiS485OQ",
-  //   authDomain: "movieproject-fc07e.firebaseapp.com",
-  //   databaseURL: "https://movieproject-fc07e.firebaseio.com",
-  //   projectId: "movieproject-fc07e",
-  //   storageBucket: "",
-  //   messagingSenderId: "42818863827"
-  // };
-  // firebase.initializeApp(config);
-// create global variables
+  
 
+//  ---------------- Creat Global Variables ---------------!!!!!
       var movie;
       var giphyName;
       var alertMovie;
@@ -32,17 +25,14 @@ var getKey2;
       var actorArr = [];
       var movieTitle;
 
+//  ---------------- Inititial document ready ---------------!!!!!     
+
    $(document).ready(function() {
+//  ---------------- This function handles events when submit button is clicked ---------------!!!!!
 
-      // This function handles events where submit button is clicked
       $("#submitId").on("click", function(event) {
-
-        function search_function(searchInput){
-          // Do your search action here
-          alert(searchInput);
-        };
-        $(function(){
-          $("#searchForm").on("submit", function(e){
+          event.preventDefault();
+        $(function(){ $("#searchForm").on("submit", function(e){
             search_function($("#searchInput").val());
             e.preventDefault(); // Prevents submitting in most browsers
             return false; // Prevents submitting in some other browsers
@@ -51,9 +41,7 @@ var getKey2;
 
         event.preventDefault();
         giphyName = $("#searchInput").val();
-
-        // This line grabs the input from the textbox
-        movie = $("#searchInput").val().trim();
+        movie = $("#searchInput").val().trim();// This line grabs the input from the textbox
         favs.push(movie);
         sugs.push(movie);
         
@@ -63,39 +51,31 @@ var getKey2;
         $("#searchInput").val("");
       });// ends submit
 
-// --------------------------------------------------- functions---------------------------!!!!
+//  ------------------------------ Functions Below -----------------------------!!!!!
 
-      // Function for displaying the movie info
+//  ---------------- Function for displaying the movie info ---------------!!!!!
+
       function displayMovieInfo(){
         
-        
-          // Here we construct our URL
-          var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
-            console.log(queryURL);
+        //  ---------------- Ajax call for Omdb ---------------!!!!!
+          var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=full&apikey=40e9cece";
+
           $.ajax({
               url: queryURL,
               method: "GET"
           }).done(function(response) {
-                    // Creates a div to hold the movie
-          // Retrieves the Rating Data
-         console.log(response)
+    
               $("#actorDiv").empty()
          
               var col = "col-xs-3 col-sm-3 col-md-3 col-lg-3"
               actors = response.Actors;
               actorArr = actors.split(",");
               for (var i = actorArr.length - 1; i >= 0; i--) {
-
-              // $("#actorDiv").append("<div class = 'container-fluid actimg " + col +"' id = "  +
-              //  "'" + actorArr[i] + "'>" + actorArr[i]+ "</div>");
                 var res=actorArr[i].replace(/ /g, "_");
                 var wikiURL= "https://en.wikipedia.org/wiki/"+res+"?index.php?action=render";
-                  console.log(wikiURL);
                 $("#actorDiv").append("<div class='container-fluid actimg" + col +"'><a href = "+ wikiURL + " target = '_blank'>" + actorArr[i] + "</a></div>");
               };
               
-              
-            console.log(actorArr)
               movieTitle = response.Title;
               rating = response.Rated;
               release = response.Released;
@@ -103,6 +83,8 @@ var getKey2;
               poster = response.Poster;
              
                 var overview = JSON.stringify(response);
+
+                //  ---------------- Dynamically write to Divs ---------------!!!!!
         
            $(".trailer").html("<h3>Rating: " + rating +"<br>Released: " + release +"<br>Plot: "
            + plot + "</h3>");
@@ -115,7 +97,7 @@ var getKey2;
 
           $("#htmlTitle").html("<h2>" + movieTitle + "</h2>");
 
-          // Pull trailer playlist from YouTube
+          //  ---------------- Pull trailer playlist from YouTube ---------------!!!!!
 
           $("#playDiv").html("<iframe width='560' height='315' src='https://youtube.com/embed?listType=search&list=" +
            movie + "+trailer' frameborder='0' allowfullscreen></iframe>");
@@ -130,8 +112,7 @@ var getKey2;
       
   
 
-      // ---------------------------------Below is Giphy API------------!!!!!!
-
+                //  ---------------- Below is Giphy API ---------------!!!!!
 
       function displayGifs(){
         $("#giphyDiv").empty();
@@ -168,112 +149,105 @@ var getKey2;
                 }
         });
       };//end of displayGifs
+            //  ---------------- Render Suggestion Buttons ---------------!!!!!
 
-// render suggestions buttons !!!!!!!!!
       function renderSugButtons() {
-        $("#suglocDiv").empty();
-        dataRef2.on("child_added", function(childSnapShot){
-            var sug = childSnapShot.val().favoritesFIRE;
-       $("#suglocDiv").append("<div class = 'clearDiv' id = '" + childSnapShot.key() +"'><button data-name = '" +
-           sug + "' class = 'sugMovie btn btn-info'>" +
-           sug + "</button><br><div data-name = '" + sug + "' class = 'sugclear'>X</div</div")
-               
-           });//end of child added
-          };//end of render
 
-          renderSugButtons();
-      //render buttons function
+           $("#suglocDiv").empty();
+              dataRef2.on("child_added", function(childSnapShot){
+              var sug = childSnapShot.val().favoritesFIRE;
+              $("#suglocDiv").append("<div class = 'clearDiv' id = '" + childSnapShot.key() +"'><button data-name = '" +
+              sug + "' class = 'sugMovie btn btn-info'>" +
+              sug + "</button><br><div data-name = '" + sug + "' class = 'sugclear'>X</div</div")
+                 
+             });//end of child added
+      };//end of render
+
+
+          renderSugButtons();// initial render Suggestion call
+
+              //  ---------------- Render Buttons Function ---------------!!!!!
+
       function renderButtons() {
 
-
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise we will have repeat buttons)
           $("#favoritesDiv").empty();
-           dataRef.on("child_added", function(childSnapShot){
+            dataRef.on("child_added", function(childSnapShot){
             var fav = childSnapShot.val().favoritesFIRE;
-            // console.log(fav);
-            // console.log(childSnapShot.key())
-          $("#favoritesDiv").append("<div class = 'clearDiv' id = '" + childSnapShot.key() +"'><button data-name = '" +
-           fav + "' class = 'favMovie btn btn-info'>" +
-           fav + "</button><br><div data-name = '" + fav + "' class = 'clear'>X</div</div")
+
+          $("#favoritesDiv").append("<div class = 'clearDiv' id = '" + childSnapShot.key() +
+             "'><button data-name = '" +
+             fav + "' class = 'favMovie btn btn-info'>" +
+             fav + "</button><br><div data-name = '" + fav + "' class = 'clear'>X</div</div")
                
            });//end of child added
-          };//end of render
+      };//end of render
 
-     // Render buttons inital 
+     // Render buttons inital call
       renderButtons();
 
-// ------------------------------------------------ click functions --------------------!!!!
+                                
+                  //  ---------------- Click functions below ---------------!!!!!
 
-      // suggestion movies click
+       //  ---------------- Suggestion movies click ---------------!!!!!
 
         $('body').on('click', '.sugMovie', function() {
           movie = $(this).text();
-          console.log(this)
-        console.log(movie)
-        giphyName = movie;
+          giphyName = movie;
           displayGifs();
           displayMovieInfo();
         });//end of sug click function
 
 
-      // favorit movies on click
-      $('body').on('click', '.favMovie', function() {
+       //  ---------------- Favorite Movies Click ---------------!!!!!
+        $('body').on('click', '.favMovie', function() {
           movie = $(this).text();
-          console.log(this)
-        console.log(movie)
-        giphyName = movie;
+          giphyName = movie;
           displayGifs();
           displayMovieInfo();
           $("#favbtnDiv").css("display" , "none")
         });//end of favorite click function
 
 
-//    --------------------suggestion button _-----
-       $('body').on('click', '#sugbtn', function() {
+ //  ---------------- Add Suggestion Button click ---------------!!!!!
+        $('body').on('click', '#sugbtn', function() {
                   keyHolder2 = dataRef2.push({
                   favoritesFIRE: movie,
-        });//end of push
+                  });//end of push
 
-     //Favorit button clicked
+              renderSugButtons()
 
-                })//end of push
+        })//end of sugbtn click function
 
+  //  ---------------- Add Favorite Button Click ---------------!!!!!
         $('body').on('click', '#favbtn', function() {
-          event.preventDefault();
                   keyHolder = dataRef.push({
                   favoritesFIRE: movie,
                   });//end of push
-                  
-
-        renderSugButtons()
+        
         renderButtons();
         
         });//end of favbtn
-         //clear favorite
+        //  ---------------- Clear Suggestion  ---------------!!!!!
+                      
         $('body').on('click', '.sugclear', function() {
-          
-          var test = $(this).attr('data-name');
-          // console.log(test)
-          $(this).closest ('button').remove();
-          getKey2 = $(this).parent().attr('id');
-          dataRef2.child(getKey2).remove();
-          renderSugButtons();
-           })
+                
+                $(this).closest ('button').remove();
+                getKey2 = $(this).parent().attr('id');
+                dataRef2.child(getKey2).remove();
+                renderSugButtons();
+                
+        });//end Suggestion clear
 
-        //clear favorite
+          //  ---------------- Clear Favorites ---------------!!!!!
         $('body').on('click', '.clear', function() {
+               
+                $(this).closest ('button').remove();
+                getKey = $(this).parent().attr('id');
+                dataRef.child(getKey).remove();
+                renderButtons();
+           });// end favorite clear
          
-          var test = $(this).attr('data-name');
-          // console.log(test)
-          $(this).closest ('button').remove();
-          getKey = $(this).parent().attr('id');
-          console.log(getKey);
-          dataRef.child(getKey).remove();
-          renderButtons();
-           })
-         
-         // pause gif function
+        //  ---------------- Pause Gif Function ---------------!!!!!
           $('body').on('click', '.gif', function() {
             var src = $(this).attr("src");
           if($(this).hasClass('playing')){
